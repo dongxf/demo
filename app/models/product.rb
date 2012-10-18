375c04e21124a49a044ff9461c3d61148fb27283
Franklin Dong <dongxf@gmail.com>
2012-10-18 09:16:12 +0800

    Store added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,11 @@
 #encoding:utf-8
 class Product < ActiveRecord::Base
+  default_scope :order => 'title'
   attr_accessible :description, :image_url, :price, :title
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :uniqueness => true
+  validates :title, :length => {:minimum => 10, :message => '原因，就不告诉你'}
   validates :image_url, :format => {
     :with => %r{\.(gif|jpg|png)$}i,
     #:message => 'must be a URL for GIF, JPG or PNG image.'
